5a65152c001d3e12ea52a25b39bc5a56a79f67f4
github username <github email address>
2017-09-03 03:37:27 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,31 @@
+module Memorable
+
+  module ClassMethods
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
+
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+
+  end
+
+end
